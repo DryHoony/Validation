0711eb4b6c2fdbc9f18a8c28bad171d978ccd7bf
Hoony <xognsdl710@naver.com>
2023-04-29 02:03:47 +0900

    2023.04.29 <5. 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술> 4.) 검증1 - Validation - 커밋 Test

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ItemServiceApplication {
-	// 커밋확인
+	// 커밋 Test
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
